e1ffd1ee8ae5d36181574d622852ec264f2342f0
qiang_liu <lq.linknet@gmail.com>
2015-10-30 11:13:37 +0800

    ok

--- CommunityCenter/CommunityCenter/Activitys/ServerType/Errand/ErrandMainActivity.cs
+++ CommunityCenter/CommunityCenter/Activitys/ServerType/Errand/ErrandMainActivity.cs
@@ -13,14 +13,30 @@ using Android.Widget;
 
 namespace CommunityCenter.Activitys.ServerType.Errand
 {
-	[Activity (Label = "ErrandMainActivity")]			
+	[Activity (Theme = "@style/MyCustomTheme")]			
 	public class ErrandMainActivity : Activity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
 			// Create your application here
+			RequestWindowFeature (WindowFeatures.CustomTitle);
+			SetContentView (Resource.Layout.ErrandMainLayout);
+			Window.SetFeatureInt (WindowFeatures.CustomTitle, Resource.Layout.custom_title_bar);
+			// Create your application here
+			InitView();
+		}
+		private void InitView()
+		{
+			//设置标题栏
+			var img_header_back = FindViewById<ImageView> (Resource.Id.img_header_back);
+			img_header_back.Click += (sender, e) => 
+			{
+				this.Finish();
+				OverridePendingTransition(Android.Resource.Animation.SlideInLeft,Android.Resource.Animation.SlideOutRight);
+			};
+			var tv_header_title = FindViewById<TextView> (Resource.Id.tv_header_title);
+			tv_header_title.Text = "跑腿服务";
 		}
 	}
 }
--- CommunityCenter/CommunityCenter/Activitys/ServerType/Nurse/NurseMainActivity.cs
+++ CommunityCenter/CommunityCenter/Activitys/ServerType/Nurse/NurseMainActivity.cs
@@ -13,14 +13,30 @@ using Android.Widget;
 
 namespace CommunityCenter.Activitys.Nurse
 {
-	[Activity (Label = "NurseMainActivity")]			
+	[Activity (Theme = "@style/MyCustomTheme")]			
 	public class NurseMainActivity : Activity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
 			// Create your application here
+			RequestWindowFeature (WindowFeatures.CustomTitle);
+			SetContentView (Resource.Layout.NurseMainLayout);
+			Window.SetFeatureInt (WindowFeatures.CustomTitle, Resource.Layout.custom_title_bar);
+			// Create your application here
+			InitView();
+		}
+		private void InitView()
+		{
+			//设置标题栏
+			var img_header_back = FindViewById<ImageView> (Resource.Id.img_header_back);
+			img_header_back.Click += (sender, e) => 
+			{
+				this.Finish();
+				OverridePendingTransition(Android.Resource.Animation.SlideInLeft,Android.Resource.Animation.SlideOutRight);
+			};
+			var tv_header_title = FindViewById<TextView> (Resource.Id.tv_header_title);
+			tv_header_title.Text = "护理服务";
 		}
 	}
 }
