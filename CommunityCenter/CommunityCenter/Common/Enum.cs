7eeb2091f40cd5d6f423c290367901bbfbcb4267
qiang_liu <lq.linknet@gmail.com>
2015-11-12 18:53:51 +0800

    serviceType

--- CommunityCenter/CommunityCenter/Common/Enum.cs
+++ CommunityCenter/CommunityCenter/Common/Enum.cs
@@ -46,6 +46,51 @@ namespace CommunityCenter
 		ChargeTraffic=19,//交通违章缴费
 		ChargeTv=20,//有限电视缴费
 		ChargeWater=21//水缴费
+			
+	}
+
+	public enum ServiceType
+	{
+		Admin=0,//管理员
+		Errand=1,//跑腿
+		HouseKeep=2,//家政
+		Nurse=3,//护理
+		Nurse_Admission=4,//入院护理
+		Nurse_LateMorning=5,//晨晚护理
+		Nurse_Foot=6,//足疗护理
+		Nurse_Scrapping=7,//刮痧护理
+		Nurse_Mouth=8,//口腔护理
+		Nurse_Clinical=9,//临床护理
+		Nurese_Skin=10,//皮肤护理
+		HouseKeep_Wash=11,//洗衣
+		HouseKeep_Clean=12,//保洁
+		HouseKeep_Deal=13,//买卖
+		HouseKeep_Cook=14,//做饭
+		HouseKeep_SchoolRun=15,//接送小孩上学
+		HouseKeep_Moon=16,//月嫂
+		Errand_OneToTwo=17,//1-2km
+		Errand_TwoToThree=18,//2-3km,
+		Errand_ThreeToFour,//3-4Km,
+		Errand_FourToFive,//4-5km,
+		Errand_FiveToSix,//5-6km,
+		Errand_SixMore//6km以上
+
+	}
+
+	/// <summary>
+	/// 一周
+	/// </summary>
+	public enum WeekType
+	{
+		Monday=1,
+		Tuesday=2,
+		Wednesday=3,
+		Thursday=4,
+		Friday=5,
+		Saturday=6,
+		Sunday=7
+
+
 	}
 
 }
